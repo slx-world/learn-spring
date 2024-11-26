c16a7530b33639dc9b3d08a245aabe66ec438b8e
slx-world <slx9920@outlook.com>
2024-11-27 07:33:15 +0800

    first commit

---
+++ demo01-quickstart/src/main/java/com/itheima/dao/impl/BookDaoImpl.java
@@ -0,0 +1,17 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.BookDao;
+
+/**
+ * @Author: Song Laixiong
+ * @Create: 2024-11-27
+ * @Description:
+ */
+
+public class BookDaoImpl implements BookDao {
+
+    @Override
+    public void save() {
+        System.out.println("book dao save ...");
+    }
+}
